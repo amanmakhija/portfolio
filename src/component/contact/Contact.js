72643447c513a6b3905a54081c1a372408543c02
Aman Makhija <amanmakhija2205@gmail.com>
2022-09-24 17:35:16 +0530

    Made forms sendable

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -10,20 +10,20 @@ export default function Contact() {
                 </div>
                 <div className='main'>
                     <p>I will come back to you as soon as I get your message!</p>
-                    <form>
+                    <form action="mailto:amanmakhija2205@gmail.com" method="post" enctype="text/plain">
                         <div className='nameInput'>
-                            <input className='input' type='text' placeholder='Your First Name' />
-                            <input className='input' type='text' placeholder='Your Last Name' />
+                            <input className='input' name='firstName' type='text' placeholder='Your First Name' />
+                            <input className='input' name='lastName' type='text' placeholder='Your Last Name' />
                         </div>
                         <div>
-                            <input className='input wideInputs' type='text' placeholder='Your Email' />
+                            <input className='input wideInputs' name='email' type='text' placeholder='Your Email' />
                         </div>
                         <div>
-                            <input className='input wideInputs' type='text' placeholder='Subject' />
+                            <input className='input wideInputs' name='subject' type='text' placeholder='Subject' />
                         </div>
                         <div className='circle3'>
                             <div>
-                                <textarea className='input message wideInputs open' type='text' rows='3' placeholder='Your Message' />
+                                <textarea className='input message wideInputs open' name='message' type='text' rows='3' placeholder='Your Message' />
                             </div>
                             <div>
                                 <input className='button' type='submit' value="Send" />

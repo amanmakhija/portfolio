d46259435d3b2b4e9cdd8d1c8dce90e52d619974
Aman Makhija <68006431+amanmakhija@users.noreply.github.com>
2024-06-27 06:54:13 +0530

    design

--- src/assets/Assets.jsx
+++ src/assets/Assets.jsx
@@ -1,6 +1,6 @@
 const ASSETS = {
     wave_hand: new URL('./waving-hand.png', import.meta.url).href,
-    hero: new URL('./hero.png', import.meta.url).href,
+    hero: new URL('./hero.jpg', import.meta.url).href,
     about_primary: new URL('./image-about-primary.webp', import.meta.url).href,
     about_secondary: new URL('./image-about-secondary.png', import.meta.url).href,
     about_rotating: new URL('./image-about-rotating.svg', import.meta.url).href,
